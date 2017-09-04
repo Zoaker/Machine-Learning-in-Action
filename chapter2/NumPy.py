4ac47cfb3f57e61ad3c2d13a69c3d7a245d4c21a
ming.zheng <inspiration-sky@live.cn>
2017-09-04 19:37:30 +0800

    chapter2

---
+++ chapter2/NumPy.py
@@ -0,0 +1,6 @@
+import KNN
+import matplotlib
+import matplotlib.pyplot as plt
+# datingDataMat, datingLabels = KNN.file2matrix('data.txt')
+# normMat, ranges, minVals = KNN.autoNorm(datingDataMat)
+KNN.datingClassTest( )
---
+++ chapter2/classify0.py
@@ -0,0 +1,3 @@
+from numpy import *
+fr = open("./data.txt", 'r', encoding='cp936').read()
+print (fr)
