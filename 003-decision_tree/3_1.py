a73a574bcb43f6fc0db6571fd61dbed03a3002bc
Ming Zheng (郑明) <ming.zheng@spreadtrum.com>
2017-10-28 14:21:16 +0800

    test

--- 003-decision_tree/3_1.py
+++ 003-decision_tree/3_1.py
@@ -1,1 +1,35 @@
 from math import log
+def calcShannonEnt (dataset) :
+    numEntries = len(dataset)
+    labelCounts = {}
+    for featVec in dataset :
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return  shannonEnt
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet, labels
+
+def splitDataSet(dataSet, axis, value) :
+    retDataSet = []
+    for featVEC in dataSet :
+        if featVEC[axis] == value:
+            reducedFeatVec = featVEC[:axis]
+            reducedFeatVec.extend(featVEC[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+myDat, labels=createDataSet()
+value = splitDataSet(myDat, 0, 1)
+print (value)
